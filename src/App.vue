27f14ffcf6304d163d23a12e73d9ca9cf154b426
yuansuixin <muqing@micous.com>
2021-03-18 17:35:14 +0800

    feat header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="container ">
+  <div class="container">
+    <global-header :user="currentUser"></global-header>
     <column-list :list="list"></column-list>
   </div>
 </template>
@@ -7,6 +8,7 @@
 import { defineComponent } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
 const testData: ColumnProps[] = [
   {
     id: 1,
@@ -21,15 +23,34 @@ const testData: ColumnProps[] = [
     description: '这是test2的专栏',
     avatar:
       '//cdn.micoworld.net/web/activity/20210314/assets/images/rank-active.png'
+  },
+  {
+    id: 2,
+    title: 'test2专栏',
+    description: '这是test2的专栏',
+    avatar:
+      '//cdn.micoworld.net/web/activity/20210314/assets/images/rank-active.png'
+  },
+  {
+    id: 2,
+    title: 'test2专栏',
+    description: '这是test2的专栏',
+    avatar:
+      '//cdn.micoworld.net/web/activity/20210314/assets/images/rank-active.png'
   }
 ]
+const currentUser: UserProps = {
+  isLogin: false
+}
 export default defineComponent({
   components: {
-    ColumnList
+    ColumnList,
+    GlobalHeader
   },
   setup() {
     return {
-      list: testData
+      list: testData,
+      currentUser
     }
   }
 })
--- src/components/ColumnList.vue
+++ src/components/ColumnList.vue
@@ -1,21 +1,29 @@
 <template>
-  <ul>
-    <li v-for="column in list" :key="column.id">
-      <img :src="column.avatar" :alt="column.title" />
-      <h5>{{ column.title }}</h5>
-      <p>{{ column.description }}</p>
-      <a href="">进入专栏</a>
-    </li>
-  </ul>
+  <div class="row">
+    <div v-for="column in columnList" :key="column.id" class="col-4 mb-4">
+      <div class="card h-100 shadow-sm">
+        <div class="card-body text-center">
+          <img
+            :src="column.avatar"
+            :alt="column.title"
+            class="rounded-circle border border-light w-25 my-3"
+          />
+          <h5 class="card-title">{{ column.title }}</h5>
+          <p class="card-text text-left">{{ column.description }}</p>
+          <a href="" class="btn btn-outline-primary">进入专栏</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { computed, defineComponent, PropType } from 'vue'
 
 export interface ColumnProps {
   id: number
   title: string
-  avatar: string
+  avatar?: string
   description: string
 }
 
@@ -26,6 +34,17 @@ export default defineComponent({
       type: Array as PropType<ColumnProps[]>, // ColumnProps类型的数组，这样是用断言，可以在模板和setup中都能获取到类型
       required: true
     }
+  },
+  setup(props) {
+    const columnList = computed(() => {
+      return props.list.map(column => {
+        if (!column.avatar) {
+          column.avatar = require('@/assets/logo.png')
+        }
+        return column
+      })
+    })
+    return { columnList }
   }
 })
 </script>
