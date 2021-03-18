7ece8d16ddbe817f732c7663302a574cf1a70c4c
yuansuixin <muqing@micous.com>
2021-03-18 16:15:22 +0800

    feat columnlist

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="container ">
+    <column-list :list="list"></column-list>
   </div>
-  <router-view/>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1专栏',
+    description: '这是test1的专栏',
+    avatar:
+      '//cdn.micoworld.net/web/activity/20210314/assets/images/rank-active.png'
+  },
+  {
+    id: 2,
+    title: 'test2专栏',
+    description: '这是test2的专栏',
+    avatar:
+      '//cdn.micoworld.net/web/activity/20210314/assets/images/rank-active.png'
+  }
+]
+export default defineComponent({
+  components: {
+    ColumnList
+  },
+  setup() {
+    return {
+      list: testData
+    }
+  }
+})
+</script>
+<style></style>
---
+++ src/components/ColumnList.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul>
+    <li v-for="column in list" :key="column.id">
+      <img :src="column.avatar" :alt="column.title" />
+      <h5>{{ column.title }}</h5>
+      <p>{{ column.description }}</p>
+      <a href="">进入专栏</a>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+export interface ColumnProps {
+  id: number
+  title: string
+  avatar: string
+  description: string
+}
+
+export default defineComponent({
+  name: 'ColumnList',
+  props: {
+    list: {
+      type: Array as PropType<ColumnProps[]>, // ColumnProps类型的数组，这样是用断言，可以在模板和setup中都能获取到类型
+      required: true
+    }
+  }
+})
+</script>
+
+<style></style>
